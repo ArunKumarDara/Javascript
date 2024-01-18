334a6e67523ff4eac77dc29c9c9b7a401ed89623
Arun Kumar <mymailarun32@gmail.com>
2024-01-18 22:13:00 +0530

    learn about variables

---
+++ variables&Datatypes/variables.js
@@ -0,0 +1,20 @@
+// variables are like containers which can store different values and items
+// var is a old way of declaring variables
+// re-declaring same variable
+var a = 10;
+console.log(a);
+var a = "arun kumar";
+console.log(a);
+
+//  es6 / 2015
+// let and const is a new way of declaring variables
+// let allows to re-initiaalize but not allow to re-declare
+let b = 10;
+console.log(b);
+b = "arun kumar";
+console.log(b);
+
+// const is a new way of declaring variables which can not be re-initialized or re-declared
+const c = 20;
+c = 30;
+console.log(c);
